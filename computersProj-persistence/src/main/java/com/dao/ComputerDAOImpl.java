b09737ec44cf3074613f7f34f2a52071cee3d49f
jeromebel <jerome.bellier@cpe.fr>
2014-03-06 15:37:19 +0100

    REALESE 3.0.0 switch to jdbcTemplate

--- computersProj-persistence/src/main/java/com/dao/ComputerDAOImpl.java
+++ computersProj-persistence/src/main/java/com/dao/ComputerDAOImpl.java
@@ -1,23 +1,20 @@
 package com.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.sql.Types;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.jolbox.bonecp.BoneCPDataSource;
-import com.om.Company;
 import com.om.Computer;
 import com.servlet.wrapper.PageWrapper;
 
@@ -25,119 +22,121 @@ import com.servlet.wrapper.PageWrapper;
 public class ComputerDAOImpl implements ComputerDAO {
 	final Logger log = LoggerFactory.getLogger(ComputerDAOImpl.class);
 
+	final String FILTER_NAME = "filterName";
+	final String FILTER_ID = "filterId";
+	final String LIMIT = "limit";
+	final String OFFSET = "offset";
+	final String NAME  = "name";
+	final String INTRODUCED = "introduced";
+	final String DISCONTINUED = "discontinued";
+	final String COMPANY_ID = "company_id";
+	
+	final String SQL_READ_TOTAL_COUNT = "SELECT COUNT(*) FROM computer;";
+	final String SQL_READ_TOTAL_COUNT_BY_NAME = "SELECT COUNT(*) "
+			+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
+			+ " WHERE c.name LIKE :filterName OR f.name LIKE :filterName ;";
+	
+	final String SQL_READ_ALL = "SELECT c.id, c.name, c.company_id, c.introduced, c.discontinued , f.name"
+			+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id ";
+	final String SQL_READ_ALL_BY_NAME = "SELECT c.id,  c.name, c.company_id , c.introduced, c.discontinued ,f.name"
+			+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
+			+ " WHERE c.name LIKE :filterName OR f.name LIKE :filterName ";
+	final String SQL_READ_BY_ID = "SELECT c.id, c.name, c.company_id, c.introduced, c.discontinued , f.name"
+			+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
+			+ " WHERE c.id = :filterId ;";
+	
+	final String SQL_DELETE = "DELETE FROM computer WHERE id=:filterId";
+	final String SQL_UPDATE = "UPDATE computer SET  name= :name," + " introduced= :introduced," + " discontinued= :discontinued,"
+			+ " company_id = :company_id" + " WHERE id= :filterId";
+	final String SQL_CREATE = "INSERT INTO computer (name , introduced , discontinued , company_id) "
+			+ "VALUES(?,?,?,?)";
+
 	@Autowired
 	private BoneCPDataSource dataSource;
 
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	private JdbcTemplate jdbcTemplate;
+
+	private JdbcTemplate getNamedParameterJdbcTemplate;
+	
+	private JdbcTemplate getJdbcTemplate(){
+		if(jdbcTemplate == null)
+			jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		return jdbcTemplate;
+	}
+	
+	private NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(){
+		if(namedParameterJdbcTemplate == null)
+			namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+		return namedParameterJdbcTemplate;
+	}
+	
 	@Override
 	public Integer readTotalCount() throws SQLException {
-		PreparedStatement stmt = null;
-		Connection cn = DataSourceUtils.getConnection(dataSource);
-
-		stmt = cn.prepareStatement("SELECT COUNT(*) "
-				+ " FROM computer;");
-		ResultSet rs = (ResultSet) stmt.executeQuery();
-		while (rs.next()) {
-			Integer result = rs.getInt(1);
-			return result;
-		}
-
-		if (stmt != null)
-			stmt.close();
-		
-		if (rs != null)
-			rs.close();
-
-		return null;
+		return getJdbcTemplate().queryForInt(SQL_READ_TOTAL_COUNT);
 	}
 
 	@Override
 	public Integer readTotalCountFilterByName(String name) throws SQLException {
-		PreparedStatement stmt = null;
-		Connection cn = DataSourceUtils.getConnection(dataSource);
-
-		stmt = (PreparedStatement) cn
-				.prepareStatement("SELECT COUNT(*) "
-						+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
-						+ " WHERE c.name LIKE ? OR f.name LIKE ? ;");
-		stmt.setString(1, "%" + name + "%");
-		stmt.setString(2, "%" + name + "%");
-		ResultSet rs = (ResultSet) stmt.executeQuery();
-		while (rs.next()) {
-			Integer result = rs.getInt(1);
-			return result;
-		}
-
-		if (stmt != null)
-			stmt.close();
-		
-		if (rs != null)
-			rs.close();
-
-		return null;
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue(FILTER_NAME,"%"+name+"%");
+		return getNamedParameterJdbcTemplate().queryForObject(SQL_READ_TOTAL_COUNT_BY_NAME , namedParameters,Integer.class);
 	}
 
 	@Override
 	public List<Computer> readByPage(PageWrapper page)
-			throws SQLException {
+			throws SQLException {		
 
-		Connection cn = DataSourceUtils.getConnection(dataSource);
-		PreparedStatement stmt = null;
+		int limit = page.getComputerPerPage();
+		int offset = (page.getPageNumber() - 1) * page.getComputerPerPage();
 
-		stmt = (PreparedStatement) cn
-				.prepareStatement("SELECT c.id, c.name, c.company_id, c.introduced, c.discontinued , f.name"
-						+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
+		String sql = SQL_READ_ALL
 						+ " ORDER BY "
 						+ page.getOrderedBy()
 						+ " "
-						+ page.getOrderDirection() + " LIMIT ? OFFSET ? ");
-		int limit = page.getComputerPerPage();
-		stmt.setInt(1, limit);
-		int offset = (page.getPageNumber() - 1) * page.getComputerPerPage();
-		stmt.setInt(2, offset);
-
-		return getComputers(cn, stmt);
-
+						+ page.getOrderDirection() + " LIMIT :limit OFFSET :offset";
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue(LIMIT, limit);
+		namedParameters.addValue(OFFSET,offset);	
+		
+		log.debug(namedParameters.toString());
+				
+		return getNamedParameterJdbcTemplate().query(sql, namedParameters, new ComputerRowMapper());
 	}
 
 	@Override
 	public List<Computer> readFilterByName(PageWrapper page)
 			throws SQLException {
 
-		Connection cn = DataSourceUtils.getConnection(dataSource);
+		int limit = page.getComputerPerPage();
+		int offset = (page.getPageNumber() - 1) * page.getComputerPerPage();
 
-		if ("".equals(page.getFilterName()))
-			return this.readByPage(page);
-
-		PreparedStatement stmt = null;
-
-		stmt = (PreparedStatement) cn
-				.prepareStatement("SELECT c.id,  c.name, c.company_id , c.introduced, c.discontinued ,f.name"
-						+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
-						+ " WHERE c.name LIKE ? OR f.name LIKE ? "
+		String sql = SQL_READ_ALL_BY_NAME
 						+ " ORDER BY "
 						+ page.getOrderedBy()
 						+ " "
-						+ page.getOrderDirection() + " LIMIT ? OFFSET ? ;");
-		stmt.setString(1, "%" + page.getFilterName() + "%");
-		stmt.setString(2, "%" + page.getFilterName() + "%");
-		stmt.setInt(3, page.getComputerPerPage());
-		stmt.setInt(4, (page.getPageNumber() - 1) * page.getComputerPerPage());
-
-		return getComputers(cn, stmt);
+						+ page.getOrderDirection() + " LIMIT :limit OFFSET :offset";
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue(FILTER_NAME,"%"+page.getFilterName()+"%");
+		namedParameters.addValue(LIMIT, limit);
+		namedParameters.addValue(OFFSET, offset);		
+				
+		return getNamedParameterJdbcTemplate().query(sql, namedParameters, new ComputerRowMapper());
 
 	}
 
 	@Override
 	public Computer readFilterByID(Long id) throws SQLException {
 
-		PreparedStatement stmt = null;
-		Connection cn = DataSourceUtils.getConnection(dataSource);
-		stmt = (PreparedStatement) cn
-				.prepareStatement("SELECT c.id, c.name, c.company_id, c.introduced, c.discontinued , f.name"
-						+ " FROM computer c LEFT JOIN company f ON c.company_id = f.id "
-						+ " WHERE c.id = ? ;");
-		stmt.setLong(1, id);
-		List<Computer> computers = getComputers(cn, stmt);
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue(FILTER_ID, id);
+		
+		List<Computer> computers = getNamedParameterJdbcTemplate().query(SQL_READ_BY_ID, namedParameters, new ComputerRowMapper());
+		
 		if (computers.size() != 1) {
 			log.error("id of the computer is not found");
 			return null;
@@ -148,134 +147,70 @@ public class ComputerDAOImpl implements ComputerDAO {
 
 	@Override
 	public void delete(String id) throws NumberFormatException, SQLException {
-		StringBuilder strBuildSQL = new StringBuilder();
-		strBuildSQL.append("DELETE FROM computer ");
-		strBuildSQL.append(" WHERE ").append(" id=")
-				.append(Integer.valueOf(id));
-
-		Connection cn = null;
-		PreparedStatement stmt = null;
-		cn = DataSourceUtils.getConnection(dataSource);
-		stmt = (PreparedStatement) cn.prepareStatement("DELETE FROM computer  "
-				+ " WHERE id=?");
-		stmt.setInt(1, Integer.valueOf(id));
-		stmt.executeUpdate();
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue(FILTER_ID, id);
+		getNamedParameterJdbcTemplate().update(SQL_DELETE, namedParameters);
 
 		log.info("Computer num " + id + " well deleted");
 
-		if (stmt != null)
-			stmt.close();
 	}
 
 	@Override
 	public void update(Computer c) throws SQLException {
 
-		Connection cn = null;
-		PreparedStatement stmt = null;
-		cn = DataSourceUtils.getConnection(dataSource);
-		stmt = (PreparedStatement) cn.prepareStatement("UPDATE computer SET "
-				+ " name= ?," + " introduced= ?," + " discontinued= ?,"
-				+ " company_id= ?" + " WHERE id= ?");
-		stmt.setString(1, c.getName());
-		stmt.setString(1, c.getName());
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		
+		namedParameters.addValue(NAME, c.getName());
 		if (c.getIntroduced() != null)
-			stmt.setTimestamp(2, new Timestamp(c.getIntroduced().getMillis()));
+			namedParameters.addValue(INTRODUCED, new Timestamp(c.getIntroduced().getMillis()));
 		else
-			stmt.setNull(2, Types.TIMESTAMP);
+			namedParameters.addValue(INTRODUCED, null);
 		if (c.getDiscontinued() != null)
-			stmt.setTimestamp(3, new Timestamp(c.getDiscontinued().getMillis()));
+			namedParameters.addValue(DISCONTINUED, new Timestamp(c.getDiscontinued().getMillis()));
 		else
-			stmt.setNull(3, Types.TIMESTAMP);
+			namedParameters.addValue(DISCONTINUED, null);
 		if ((c.getCompany() != null) && (c.getCompany().getId() != 0))
-			stmt.setInt(4, c.getCompany().getId());
+			namedParameters.addValue(COMPANY_ID, c.getCompany().getId());
 		else
-			stmt.setNull(4, Types.BIGINT);
-		stmt.setLong(5, c.getId());
-
-		stmt.executeUpdate();
+			namedParameters.addValue(COMPANY_ID, null);
+		
+		namedParameters.addValue(FILTER_ID, c.getId());
+		
+		getNamedParameterJdbcTemplate().update(SQL_UPDATE, namedParameters);
 
 		log.info("Computer num " + c.getId() + " well Update");
-
-		if (stmt != null)
-			stmt.close();
 
 	}
 
 	@Override
 	public void create(Computer c) throws SQLException {
 
-		Connection cn = null;
-		PreparedStatement stmt = null;
-
-		cn = DataSourceUtils.getConnection(dataSource);
-		stmt = (PreparedStatement) cn.prepareStatement(
-				"INSERT INTO computer (name , introduced , discontinued , company_id) "
-						+ "VALUES(?,?,?,?)",
-				PreparedStatement.RETURN_GENERATED_KEYS);
-		stmt.setString(1, c.getName());
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		
+		namedParameters.addValue(NAME, c.getName());
 		if (c.getIntroduced() != null)
-			stmt.setTimestamp(2, new Timestamp(c.getIntroduced().getMillis()));
+			namedParameters.addValue(INTRODUCED, new Timestamp(c.getIntroduced().getMillis()));
 		else
-			stmt.setNull(2, Types.TIMESTAMP);
+			namedParameters.addValue(INTRODUCED, null);
 		if (c.getDiscontinued() != null)
-			stmt.setTimestamp(3, new Timestamp(c.getDiscontinued().getMillis()));
+			namedParameters.addValue(DISCONTINUED, new Timestamp(c.getDiscontinued().getMillis()));
 		else
-			stmt.setNull(3, Types.TIMESTAMP);
+			namedParameters.addValue(DISCONTINUED, null);
 		if ((c.getCompany() != null) && (c.getCompany().getId() != 0))
-			stmt.setInt(4, c.getCompany().getId());
+			namedParameters.addValue(COMPANY_ID, c.getCompany().getId());
 		else
-			stmt.setNull(4, Types.BIGINT);
+			namedParameters.addValue(COMPANY_ID, null);
 
-		stmt.executeUpdate();
-		ResultSet rs = stmt.getGeneratedKeys();
-		while (rs.next())
-			c.setId(rs.getLong(1));
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		
+		getNamedParameterJdbcTemplate().update(SQL_CREATE, namedParameters , keyHolder);
+
+		c.setId(keyHolder.getKey().intValue());
 
 		log.info("Computer num " + c.getId() + " well created");
 
-		if (stmt != null)
-			stmt.close();
 
-	}
-
-	private List<Computer> getComputers(Connection cn, PreparedStatement stmt)
-			throws SQLException {
-
-		List<Computer> allComputers = new ArrayList<Computer>();
-		ResultSet rs = null;
-		rs = (ResultSet) stmt.executeQuery();
-		while (rs.next()) {
-			Computer c = new Computer();
-
-			c.setId(rs.getInt(1));
-
-			c.setName(rs.getString(2));
-
-			Company comp = new Company();
-			comp.setId(rs.getInt(3));
-			comp.setName(rs.getString(6));
-
-			c.setCompany(comp);
-
-			if (rs.getTimestamp(4) == null)
-				c.setIntroduced(null);
-			else
-				c.setIntroduced(new DateTime(rs.getTimestamp(4)));
-
-			if (rs.getTimestamp(5) == null)
-				c.setDiscontinued(null);
-			else
-				c.setDiscontinued(new DateTime(rs.getTimestamp(5)));
-			allComputers.add(c);
-		}
-
-		if (rs != null)
-			rs.close();
-
-		if (stmt != null)
-			stmt.close();
-
-		return allComputers;
 	}
 
 }
